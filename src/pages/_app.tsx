5628cd0a85a21ad0d77a9b2ddca9fcff48ba180b
Andreas Trolle <andreas.trolle@hotmail.com>
2023-03-15 19:27:19 +0100

    usermaven

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { Work_Sans } from "@next/font/google";
+import { usermavenClient, UsermavenClient } from "@usermaven/sdk-js";
 import { type AppType } from "next/dist/shared/lib/utils";
+import router from "next/router";
+import { useEffect } from "react";
 
 import "../styles/globals.css";
 
@@ -9,6 +12,21 @@ const workSans = Work_Sans({
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Init Usermaven
+    const usermaven: UsermavenClient = usermavenClient({
+      key: "YOUR_PROJECT_KEY",
+      tracking_host: "https://events.usermaven.com",
+    });
+
+    // Track page views
+    const handleRouteChange = () => usermaven.track("pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <>
       <style jsx global>{`
